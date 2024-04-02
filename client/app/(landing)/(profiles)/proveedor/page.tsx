3cdf4beb8e762265ac9d1515f76e73d0d4ab35a3
spalacioj <samu43221@gmail.com>
2024-04-02 18:42:05 -0500

    Inicializando componente de servicios

--- client/app/(landing)/(profiles)/proveedor/page.tsx
+++ client/app/(landing)/(profiles)/proveedor/page.tsx
@@ -1,12 +1,41 @@
 import LayoutProfile from "../../layout";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import Image from 'next/image';
+import calendar from "@/public/calendar.svg";
+import cancel from "@/public/cancel.svg";
+import { Servicio } from "@/components/card/servicio";
 
 const ProveedorPage = () => {
     const titulo = "Proveedor";
-
     return (
         <LayoutProfile titulo={titulo}>
             <div>
-                asd
+                <h2 className="text-center text-5xl font-bold mt-9">Servicios</h2>
+                <p className="text-center mt-8 text-xl">Bienvenid@, seleccione el servicio que desea utilizar</p>
+            </div>
+            <div className="flex flex-row justify-center mt-16">
+                <Servicio 
+                    titulo={"Agendar Promotoria"}
+                    descripcion={"Agende una promotoria"}
+                    icon={calendar}
+                    boton={"Agendar"}
+                />
+                <Servicio 
+                    titulo={"Cancelar Promotoria"}
+                    descripcion={"Cancele una promotoria"}
+                    icon={cancel}
+                    boton={"Cancelar"}
+                />
             </div>
         </LayoutProfile>
     )
---
+++ client/components/card/servicio.tsx
@@ -0,0 +1,39 @@
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import Image from 'next/image'
+export const Servicio = ({ titulo, descripcion, icon, boton}: {
+    titulo: string;
+    descripcion: string;
+    icon: string;
+    boton: string;
+}) => {
+    return(
+        <Card className="w-[350px] m-7">
+            <CardContent className="flex flex-col items-center mt-10"> 
+                <Image 
+                    src={icon}
+                    width={140}
+                    height={140}
+                    alt="Picture of a calendar"
+                />
+            </CardContent>
+            <CardHeader>
+                <CardTitle>{titulo}</CardTitle>
+                <CardDescription>{descripcion}</CardDescription>
+            </CardHeader>
+            <CardFooter className="flex flex-col items-center">
+                <Button className="hover:bg-gray-200" variant="secondary">
+                    <Link href="/proveedor/formulario">{boton}</Link>
+                </Button>
+            </CardFooter>
+        </Card>
+    )
+}
