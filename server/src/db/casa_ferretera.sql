82af748c05b1116d0c4abecbc7605c16823ef26d
spalacioj <samu43221@gmail.com>
2024-05-08 12:18:17 -0500

    Actualizando script de la base de datos

--- server/src/db/casa_ferretera.sql
+++ server/src/db/casa_ferretera.sql
@@ -28,7 +28,7 @@ CREATE TABLE `auxmercadeo` (
   `contrasena` varchar(100) NOT NULL,
   `nombre` varchar(100) NOT NULL,
   PRIMARY KEY (`id_auxmercado`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -52,7 +52,7 @@ CREATE TABLE `empresa` (
   `id_empresa` int(11) NOT NULL AUTO_INCREMENT,
   `nombre` varchar(100) NOT NULL,
   PRIMARY KEY (`id_empresa`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -61,7 +61,7 @@ CREATE TABLE `empresa` (
 
 LOCK TABLES `empresa` WRITE;
 /*!40000 ALTER TABLE `empresa` DISABLE KEYS */;
-INSERT INTO `empresa` VALUES (1,'techvanguard');
+INSERT INTO `empresa` VALUES (1,'techvanguard'),(2,'casa ferretera'),(3,'eafit');
 /*!40000 ALTER TABLE `empresa` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -107,7 +107,7 @@ CREATE TABLE `promotor` (
   PRIMARY KEY (`id_promotor`),
   KEY `fk_proveedor` (`id_proveedor`),
   CONSTRAINT `fk_proveedor` FOREIGN KEY (`id_proveedor`) REFERENCES `proveedor` (`id_proveedor`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -116,7 +116,7 @@ CREATE TABLE `promotor` (
 
 LOCK TABLES `promotor` WRITE;
 /*!40000 ALTER TABLE `promotor` DISABLE KEYS */;
-INSERT INTO `promotor` VALUES (1,1,'dav@gmail.com','dav','$2a$10$JHu8ADe61d9r9Kwr6TTvluCvhDQYZlCazVUvaRVW2.G0RV.2Enmk6',3,'documento');
+INSERT INTO `promotor` VALUES (1,1,'dav@gmail.com','dav','$2a$10$JHu8ADe61d9r9Kwr6TTvluCvhDQYZlCazVUvaRVW2.G0RV.2Enmk6',3,'documento'),(4,1,'test2@gmail.com','daniel','$2a$10$.fHXe9s3CWUWExXF3a6ulOWZmJkyLh/TqwyS5IB1htlsqLl.w5LYq',3,'documento');
 /*!40000 ALTER TABLE `promotor` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -139,16 +139,19 @@ CREATE TABLE `promotoria` (
   `calificacion` int(11) NOT NULL,
   `registroFotos` varchar(300) NOT NULL,
   `descripcion` varchar(300) NOT NULL,
+  `id_empresa` int(11) NOT NULL,
   PRIMARY KEY (`id_promotoria`),
   KEY `fk_estado_promotoria` (`id_estado`),
   KEY `fk_promotor_promotoria` (`id_promotor`),
   KEY `fk_sede_promotoria` (`id_sede`),
   KEY `fk_proveedor_promotoria` (`id_proveedor`),
+  KEY `promotoria_empresa_FK` (`id_empresa`),
   CONSTRAINT `fk_estado_promotoria` FOREIGN KEY (`id_estado`) REFERENCES `estado` (`id_estado`),
   CONSTRAINT `fk_promotor_promotoria` FOREIGN KEY (`id_promotor`) REFERENCES `promotor` (`id_promotor`),
   CONSTRAINT `fk_proveedor_promotoria` FOREIGN KEY (`id_proveedor`) REFERENCES `proveedor` (`id_proveedor`),
-  CONSTRAINT `fk_sede_promotoria` FOREIGN KEY (`id_sede`) REFERENCES `sede` (`id_sede`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  CONSTRAINT `fk_sede_promotoria` FOREIGN KEY (`id_sede`) REFERENCES `sede` (`id_sede`),
+  CONSTRAINT `promotoria_empresa_FK` FOREIGN KEY (`id_empresa`) REFERENCES `empresa` (`id_empresa`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -157,7 +160,7 @@ CREATE TABLE `promotoria` (
 
 LOCK TABLES `promotoria` WRITE;
 /*!40000 ALTER TABLE `promotoria` DISABLE KEYS */;
-INSERT INTO `promotoria` VALUES (4,1,1,4,'2024-04-23','09:00:00','13:00:00',1,0,'','la primera promotoria'),(6,1,1,6,'2024-04-24','09:00:00','13:00:00',1,0,'',''),(7,1,1,3,'2024-04-24','09:00:00','13:00:00',1,0,'','agregando la descripcion despues de crearla'),(8,1,1,3,'2024-04-25','09:00:00','13:00:00',4,0,'',''),(9,1,1,3,'2024-04-26','09:00:00','14:00:00',1,0,'','mi primera promotoria');
+INSERT INTO `promotoria` VALUES (12,1,1,3,'2024-05-01','09:00:00','13:00:00',1,0,'','',1),(13,1,1,3,'2024-05-02','09:00:00','13:00:00',1,0,'','',1),(14,1,1,3,'2024-05-02','14:00:00','17:00:00',1,0,'','',1);
 /*!40000 ALTER TABLE `promotoria` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -177,7 +180,7 @@ CREATE TABLE `proveedor` (
   PRIMARY KEY (`id_proveedor`),
   KEY `fk_empresa_proveedor` (`id_empresa`),
   CONSTRAINT `fk_empresa_proveedor` FOREIGN KEY (`id_empresa`) REFERENCES `empresa` (`id_empresa`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -186,7 +189,7 @@ CREATE TABLE `proveedor` (
 
 LOCK TABLES `proveedor` WRITE;
 /*!40000 ALTER TABLE `proveedor` DISABLE KEYS */;
-INSERT INTO `proveedor` VALUES (1,'juanes@gmail.com','$2a$10$JbYl2vevWHmQ6mSv/PlSmeYdaqsgvn6yTrXT8TKRyAFeUzG9S7EZy','juanes',1);
+INSERT INTO `proveedor` VALUES (1,'juanes@gmail.com','$2a$10$JbYl2vevWHmQ6mSv/PlSmeYdaqsgvn6yTrXT8TKRyAFeUzG9S7EZy','juanes',1),(3,'jane_smith5678@gmail.com','$2a$10$F/qxnC/zz6SrB4NQrWSbh.JNTfKQuxwotslkZ8EWaaSSnD88TpmQO','Jane',2),(4,'alex_jones@gmail.com','$2a$10$8lwKDsi894WCq54RLVI9IOHEARSJcjLaiAghW0hbmKQvzkArqC7FC','Alex',3),(5,'john_doe@gmail.com','$2a$10$VpuLEu5lroC6vKOuY/4rGe6V5cGHIXqyl03tLxf5wTs2k8zYcdBy2','john',1),(6,'michael_wilson@gmail.com','$2a$10$d.y02RJbA79/HNFh.s0fbOlPdWR7qSs2zowNLQ45HUT1fiN6jKpeu','michael',3),(7,'chris_taylor@gmail.com','$2a$10$GRv42axJZXpKrGVnml0xU.Cl4YAs8vlXrp.nDYvpy.bzYIRNlkGEW','chris',2);
 /*!40000 ALTER TABLE `proveedor` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -230,7 +233,7 @@ CREATE TABLE `supervisor` (
   PRIMARY KEY (`id_supervisor`),
   KEY `fk_sede_supervisor` (`id_sede`),
   CONSTRAINT `fk_sede_supervisor` FOREIGN KEY (`id_sede`) REFERENCES `sede` (`id_sede`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -239,7 +242,7 @@ CREATE TABLE `supervisor` (
 
 LOCK TABLES `supervisor` WRITE;
 /*!40000 ALTER TABLE `supervisor` DISABLE KEYS */;
-INSERT INTO `supervisor` VALUES (1,3,'manin@email.com','$2a$10$vPCl5nZQ8.T9gJclFh5OlO9RxlGuxweiTawtX7TTGRPPtQIQFcOCG','manin');
+INSERT INTO `supervisor` VALUES (1,3,'manin@email.com','$2a$10$vPCl5nZQ8.T9gJclFh5OlO9RxlGuxweiTawtX7TTGRPPtQIQFcOCG','manin'),(3,1,'Emily12@email.com','$2a$10$7qfSRVHQN/RFXrUOdXMO3uNQqZVuH0XJFmBN1wT7d058CBzYglOyy','Emily'),(4,9,'amanda32@email.com','$2a$10$xEHg.VVzGrTby3RIxPi/oet871Rw7mCqG/Hys/2y5NkJ4wQ8cQ/XC','Amanda'),(5,8,'david58@email.com','$2a$10$dUq14Bcmn1uNX3xI1ZIOxOzfWbPBc.YUGepwU3EU71uuc5EootceW','David'),(6,4,'sebastian7@email.com','$2a$10$pJcMly0plf04j7W0ZnKOyuiRftu/W9zJVNL1BcukJhDULSAjvwuKu','Sebastian');
 /*!40000 ALTER TABLE `supervisor` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -256,4 +259,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-04-25 21:58:54
+-- Dump completed on 2024-05-08 12:17:04
